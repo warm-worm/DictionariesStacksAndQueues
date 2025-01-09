8ff8f56d12189cdc7e0eef596d2d8c71da792138
Aleksandra Korniak <ola.korniak@gmail.com>
2025-01-09 20:14:48 +0100

    Add files via upload

---
+++ winter_courses.py
@@ -0,0 +1,9 @@
+winter_semester = {
+   "math":60,
+   "programming":30,
+   "history":15
+}
+
+hours_sum = sum(winter_semester.values())
+
+print(f'The total number of hours in the winter semester is {hours_sum}')
