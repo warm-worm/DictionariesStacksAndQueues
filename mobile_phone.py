af6410b54a1a4815e00f3a4d5c503c269f319242
Aleksandra Korniak <ola.korniak@gmail.com>
2025-01-09 14:20:24 +0100

    Add files via upload

---
+++ mobile_phone.py
@@ -0,0 +1,10 @@
+mobile = {
+"OS":"Android",
+"Brand":"Samsung",
+"Model":"Galaxy 20FE",
+"Storage":"128GB",
+"RAM":"6GB",
+'Display':'6.5"'
+}
+for key,value in mobile.items():
+   print(f"{key} : {value}")
