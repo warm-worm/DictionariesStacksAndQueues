f7443c60296e7c33693babe190a129ada5627951
Aleksandra Korniak <ola.korniak@gmail.com>
2025-01-13 00:11:55 +0100

    Add files via upload

--- 5.23/euro.py
+++ 5.23/euro.py
@@ -1,1 +1,13 @@
+import json
 
+with open('euro.json', 'r') as file:
+    data = json.load(file)
+
+print('Date            Buying Rate     Selling Rate')
+print('============================================')
+
+for rate in data['rates']:
+    date = rate['effectiveDate']
+    buying_rate = rate['bid']
+    selling_rate = rate['ask']
+    print(f'{date}      {buying_rate}          {selling_rate}')
