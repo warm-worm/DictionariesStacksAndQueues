89addab07562e77cdc42cd7435c4273a1e25346e
Aleksandra Korniak <ola.korniak@gmail.com>
2025-01-12 22:41:28 +0100

    Add files via upload

--- 5.17/cities_info.py
+++ 5.17/cities_info.py
@@ -1,1 +1,12 @@
+import json
 
+# Open the JSON file in read mode
+with open('cities.json', 'r', encoding='utf-8') as file:
+   # Load the data from the JSON file into a variable
+   data = json.load(file)
+
+# Print the JSON data
+for city in data:
+   for key , value in city.items():
+      print(key,':',value)
+   print()
